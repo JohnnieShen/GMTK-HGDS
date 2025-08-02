ba46157ebcf5b3dc4f7a1c48692ee6514e885cf7
Johnny <shenjiaming2003@gmail.com>
2025-08-02 13:47:44 -0400

    Update KillZone.cs

--- Assets/Scripts/Props/KillZone.cs
+++ Assets/Scripts/Props/KillZone.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class KillZone : MonoBehaviour
 {
+    void Reset()
+    {
+        var col = GetComponent<Collider2D>();
+        col.isTrigger = true;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player")) 
+            return;
 
-        if (LifeManager.Instance != null)
-            LifeManager.Instance.EndCurrentLife();
+        var current = GameManager.Instance.CurrentPlayer;
+        if (other.gameObject != current) 
+            return;
+
+        Debug.Log("KillZone: live player entered – ending life");
+        LifeManager.Instance.EndCurrentLife();
     }
 }
